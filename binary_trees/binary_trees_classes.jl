e8fcb92d0ade1b039cc5dc2e30d4c9fe51317c12
MiguelMarcelino <mac.marcelino@outlook.pt>
2022-07-28 20:26:38 +0100

    Add class benchmarks

--- binary_trees/binary_trees_classes.jl
+++ binary_trees/binary_trees_classes.jl
@@ -1,16 +1,24 @@
 
-function make_tree(depth::Int64)::Tuple
-    #=  Trees or tuples, final leaves have None as values.  =#
-    return depth == 0 ? ((nothing, nothing)) :
-           ((make_tree(depth - 1), make_tree(depth - 1)))
+
+abstract type AbstractNode end
+mutable struct Node <: AbstractNode
+    left::Union{Node,Nothing}
+    right::Union{Node,Nothing}
 end
 
-function check_node(left, right)::Int64
+function make_tree(depth::Int64)::Node
+    #=  Builds a tree. Final leaves have None as values.  =#
+    return depth == 0 ? (Node(nothing, nothing)) :
+           (Node(make_tree(depth - 1), make_tree(depth - 1)))
+end
+
+function check_node(node::Node)::Int64
     #= 
         Count 1 for each node found.
         (Unpacking directly in the parameters is faster)
          =#
-    return left === nothing ? (1) : ((1 + check_node(left...)) + check_node(right...))
+    return node.left === nothing ? (1) :
+           ((1 + check_node(node.left)) + check_node(node.right))
 end
 
 function run(depth::Int64)::Int64
@@ -18,10 +26,10 @@ function run(depth::Int64)::Int64
         Makes a tree then checks it (parse all nodes and count).
         This function is global for multiprocessing purposes.
          =#
-    return check_node(make_tree(depth)...)
+    return check_node(make_tree(depth))
 end
 
-function main(requested_max_depth, min_depth = 4)
+function main(requested_max_depth::Int64, min_depth::Int64 = 4)
     max_depth = max(min_depth + 2, requested_max_depth)
     stretch_depth = max_depth + 1
     println("stretch tree of depth $(stretch_depth)\t check: $(run(stretch_depth))")
@@ -32,11 +40,9 @@ function main(requested_max_depth, min_depth = 4)
         check_sum = sum(map(run, repeat([(test_depth,)...], tree_count)))
         println("$(tree_count)\t trees of depth $(test_depth)\t check: $(check_sum)")
     end
-    println(
-        "long lived tree of depth $(max_depth)\t check: $(check_node(long_lived_tree...))",
-    )
+    println("long lived tree of depth $(max_depth)\t check: $(check_node(long_lived_tree))")
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
-    main(parse(Int, append!([PROGRAM_FILE], ARGS)[2]))
+    main(parse(Int, ARGS[1]))
 end
--- binary_trees/binary_trees_classes_oop.jl
+++ binary_trees/binary_trees_classes_oop.jl
@@ -1,16 +1,32 @@
+using ObjectOriented
 
-function make_tree(depth::Int64)::Tuple
-    #=  Trees or tuples, final leaves have None as values.  =#
-    return depth == 0 ? ((nothing, nothing)) :
-           ((make_tree(depth - 1), make_tree(depth - 1)))
+
+@oodef mutable struct Node
+    left::Union{Node,Nothing}
+    right::Union{Node,Nothing}
+
+    function new(left, right)
+        @mk begin
+            left = left
+            right = right
+        end
+    end
+
+
+end
+function make_tree(depth::Int64)::Node
+    #=  Builds a tree. Final leaves have None as values.  =#
+    return depth == 0 ? (Node(nothing, nothing)) :
+           (Node(make_tree(depth - 1), make_tree(depth - 1)))
 end
 
-function check_node(left, right)::Int64
+function check_node(node::Node)::Int64
     #= 
         Count 1 for each node found.
         (Unpacking directly in the parameters is faster)
          =#
-    return left === nothing ? (1) : ((1 + check_node(left...)) + check_node(right...))
+    return node.left === nothing ? (1) :
+           ((1 + check_node(node.left)) + check_node(node.right))
 end
 
 function run(depth::Int64)::Int64
@@ -18,10 +34,10 @@ function run(depth::Int64)::Int64
         Makes a tree then checks it (parse all nodes and count).
         This function is global for multiprocessing purposes.
          =#
-    return check_node(make_tree(depth)...)
+    return check_node(make_tree(depth))
 end
 
-function main(requested_max_depth, min_depth = 4)
+function main(requested_max_depth::Int64, min_depth::Int64 = 4)
     max_depth = max(min_depth + 2, requested_max_depth)
     stretch_depth = max_depth + 1
     println("stretch tree of depth $(stretch_depth)\t check: $(run(stretch_depth))")
@@ -32,11 +48,9 @@ function main(requested_max_depth, min_depth = 4)
         check_sum = sum(map(run, repeat([(test_depth,)...], tree_count)))
         println("$(tree_count)\t trees of depth $(test_depth)\t check: $(check_sum)")
     end
-    println(
-        "long lived tree of depth $(max_depth)\t check: $(check_node(long_lived_tree...))",
-    )
+    println("long lived tree of depth $(max_depth)\t check: $(check_node(long_lived_tree))")
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
-    main(parse(Int, append!([PROGRAM_FILE], ARGS)[2]))
+    main(parse(Int, ARGS[1]))
 end
